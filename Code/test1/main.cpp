08fb1a2949fcb44cdb885da530e09dc488da0f90
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-07-31 16:41:56 -0400

    testupdate

--- Code/test1/main.cpp
+++ Code/test1/main.cpp
@@ -1,6 +1,12 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+typedef long long int lli;
 
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+dfsfdsdfsdsf
+dsffdsdfssdf
 	return 0;
 }
