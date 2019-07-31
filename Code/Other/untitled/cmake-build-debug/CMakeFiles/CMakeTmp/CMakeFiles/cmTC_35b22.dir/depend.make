b421bf522305a8df3f4e5d661a125a0ac0a71744
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-07-31 17:00:57 -0400

    Folderize

---
+++ Code/Other/untitled/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_35b22.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_35b22.
+# This may be replaced when dependencies are built.
---
+++ Code/WAC/Problem 7/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_a282e.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_a282e.dir/feature_tests.cxx.obj"
+  "cmTC_a282e.pdb"
+  "cmTC_a282e.exe"
+  "cmTC_a282e.exe.manifest"
+  "libcmTC_a282e.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_a282e.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
