5fdabba60c6e67583679068cd2d71399bc810c69
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-08-01 18:04:41 -0400

    WINDOWSPUSH

---
+++ Problems/Duplicates/CEOI '17 P1 - One-Way Streets/main.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n,m,p;
+    cin>>n>>m>>p;
+    for(int i=0;i<m;i++){
+
+    }
+    return 0;
+}
