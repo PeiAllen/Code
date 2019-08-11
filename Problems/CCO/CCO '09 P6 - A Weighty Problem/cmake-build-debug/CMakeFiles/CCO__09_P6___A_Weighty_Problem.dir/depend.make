142dec6532ae48e5d0c05bc657a00d69860b7f9b
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-08-11 18:29:53 -0400

    pushty

---
+++ Problems/CCO/CCO '09 P6 - A Weighty Problem/cmake-build-debug/CMakeFiles/CCO__09_P6___A_Weighty_Problem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CCO__09_P6___A_Weighty_Problem.
+# This may be replaced when dependencies are built.
---
+++ Problems/Codeforces/578Div2/cmake-build-debug/CMakeFiles/578Div2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/578Div2.dir/main.cpp.obj"
+  "578Div2.pdb"
+  "578Div2.exe"
+  "578Div2.exe.manifest"
+  "lib578Div2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/578Div2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
