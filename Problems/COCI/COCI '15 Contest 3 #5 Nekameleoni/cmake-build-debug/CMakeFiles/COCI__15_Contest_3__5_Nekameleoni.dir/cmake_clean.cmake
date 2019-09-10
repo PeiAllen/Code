9d83a0e9a50e6c4a86a5aeeeb84288ee3522b479
Allen <peiall@utschools.ca>
2019-09-10 17:22:44 -0400

    pushy

---
+++ Problems/COCI/COCI '15 Contest 3 #5 Nekameleoni/cmake-build-debug/CMakeFiles/COCI__15_Contest_3__5_Nekameleoni.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COCI__15_Contest_3__5_Nekameleoni.dir/main.cpp.o"
+  "COCI__15_Contest_3__5_Nekameleoni.pdb"
+  "COCI__15_Contest_3__5_Nekameleoni"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COCI__15_Contest_3__5_Nekameleoni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/COCI/COCI '15 Contest 3 #5 Nekameleoni/cmake-build-debug/CMakeFiles/COCI__15_Contest_3__5_Nekameleoni.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for COCI__15_Contest_3__5_Nekameleoni.
+# This may be replaced when dependencies are built.
