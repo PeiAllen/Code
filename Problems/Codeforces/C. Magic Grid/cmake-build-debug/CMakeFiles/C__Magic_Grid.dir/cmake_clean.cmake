16187eda703d163374450f71030de836df4708a1
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-09-16 23:47:26 -0400

    pushy

---
+++ Problems/Codeforces/C. Magic Grid/cmake-build-debug/CMakeFiles/C__Magic_Grid.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Magic_Grid.dir/main.cpp.obj"
+  "C__Magic_Grid.pdb"
+  "C__Magic_Grid.exe"
+  "C__Magic_Grid.exe.manifest"
+  "libC__Magic_Grid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Magic_Grid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/E. Another Sith Tournament/cmake-build-debug/CMakeFiles/E__Another_Sith_Tournament.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E__Another_Sith_Tournament.dir/main.cpp.obj"
+  "E__Another_Sith_Tournament.pdb"
+  "E__Another_Sith_Tournament.exe"
+  "E__Another_Sith_Tournament.exe.manifest"
+  "libE__Another_Sith_Tournament.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E__Another_Sith_Tournament.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
