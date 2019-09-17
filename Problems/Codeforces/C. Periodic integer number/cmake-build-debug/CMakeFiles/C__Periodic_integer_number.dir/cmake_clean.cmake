a46d06c7eea725874aad657fed400a86ebb56def
Allen <peiall@utschools.ca>
2019-09-17 10:24:07 -0400

    pushy

---
+++ Problems/Codeforces/C. Periodic integer number/cmake-build-debug/CMakeFiles/C__Periodic_integer_number.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Periodic_integer_number.dir/main.cpp.o"
+  "C__Periodic_integer_number.pdb"
+  "C__Periodic_integer_number"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Periodic_integer_number.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/D. Workout plan/cmake-build-debug/CMakeFiles/D__Workout_plan.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/D__Workout_plan.dir/main.cpp.o"
+  "D__Workout_plan.pdb"
+  "D__Workout_plan"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D__Workout_plan.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
