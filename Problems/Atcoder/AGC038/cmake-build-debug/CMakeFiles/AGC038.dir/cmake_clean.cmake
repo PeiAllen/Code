fdb5e311f62b05d2ebaaf62472ecc12d4c8b09a8
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-09-21 11:29:27 -0400

    pushy

---
+++ Problems/Atcoder/AGC038/cmake-build-debug/CMakeFiles/AGC038.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AGC038.dir/main.cpp.obj"
+  "AGC038.pdb"
+  "AGC038.exe"
+  "AGC038.exe.manifest"
+  "libAGC038.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AGC038.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
