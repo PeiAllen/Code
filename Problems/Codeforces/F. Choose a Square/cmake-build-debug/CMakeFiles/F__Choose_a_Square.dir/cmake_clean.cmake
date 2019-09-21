2d42ddc3dc35f1bc7b10f9774ec08fe20ecd92d9
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-09-20 23:18:18 -0400

    pushy

---
+++ Problems/Codeforces/F. Choose a Square/cmake-build-debug/CMakeFiles/F__Choose_a_Square.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F__Choose_a_Square.dir/main.cpp.obj"
+  "F__Choose_a_Square.pdb"
+  "F__Choose_a_Square.exe"
+  "F__Choose_a_Square.exe.manifest"
+  "libF__Choose_a_Square.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/F__Choose_a_Square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/F. Choose a Square/cmake-build-debug/CMakeFiles/F__Choose_a_Square.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for F__Choose_a_Square.
+# This may be replaced when dependencies are built.
