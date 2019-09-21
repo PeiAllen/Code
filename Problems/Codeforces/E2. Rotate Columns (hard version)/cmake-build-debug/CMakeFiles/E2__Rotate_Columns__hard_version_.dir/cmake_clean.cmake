ff52659fd8d702c22e29471696dd49d90c4eb893
Allen <peiall@utschools.ca>
2019-09-20 21:03:20 -0400

    pushy

---
+++ Problems/Codeforces/E2. Rotate Columns (hard version)/cmake-build-debug/CMakeFiles/E2__Rotate_Columns__hard_version_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E2__Rotate_Columns__hard_version_.dir/main.cpp.o"
+  "E2__Rotate_Columns__hard_version_.pdb"
+  "E2__Rotate_Columns__hard_version_"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E2__Rotate_Columns__hard_version_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/e2columns/cmake-build-debug/CMakeFiles/e2columns.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/e2columns.dir/main.cpp.o"
+  "e2columns.pdb"
+  "e2columns"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/e2columns.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
