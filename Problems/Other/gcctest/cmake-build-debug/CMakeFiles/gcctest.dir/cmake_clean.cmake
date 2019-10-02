c2d30ea71e8ea4e2e5dfc532a89e25c6f519c150
Allen <peiall@utschools.ca>
2019-10-02 08:56:51 -0400

    pushy

---
+++ Problems/Other/gcctest/cmake-build-debug/CMakeFiles/gcctest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gcctest.dir/main.cpp.o"
+  "gcctest.pdb"
+  "gcctest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gcctest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Other/testgdb/cmake-build-debug/CMakeFiles/testgdb.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testgdb.dir/main.cpp.o"
+  "testgdb.pdb"
+  "testgdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testgdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
