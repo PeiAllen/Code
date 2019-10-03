4d4b2e5dc6ff095f64f872d053a2c59755eafd18
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-10-02 20:13:48 -0400

    pushy

---
+++ Problems/Codeforces/E. Special Permutations/cmake-build-debug/CMakeFiles/E__Special_Permutations.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E__Special_Permutations.dir/main.cpp.obj"
+  "E__Special_Permutations.pdb"
+  "E__Special_Permutations.exe"
+  "E__Special_Permutations.exe.manifest"
+  "libE__Special_Permutations.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E__Special_Permutations.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/F. Yet Another Substring Reverse/cmake-build-debug/CMakeFiles/F__Yet_Another_Substring_Reverse.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/F__Yet_Another_Substring_Reverse.dir/main.cpp.obj"
+  "F__Yet_Another_Substring_Reverse.pdb"
+  "F__Yet_Another_Substring_Reverse.exe"
+  "F__Yet_Another_Substring_Reverse.exe.manifest"
+  "libF__Yet_Another_Substring_Reverse.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/F__Yet_Another_Substring_Reverse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
