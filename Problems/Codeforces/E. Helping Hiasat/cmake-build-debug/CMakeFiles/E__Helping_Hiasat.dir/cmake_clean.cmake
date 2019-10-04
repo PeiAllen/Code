69b0d6b682643ebeed72e46374bbdb851ee1165f
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-10-03 22:28:34 -0400

    poushy

---
+++ Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/CMakeFiles/E__Helping_Hiasat.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E__Helping_Hiasat.dir/main.cpp.obj"
+  "E__Helping_Hiasat.pdb"
+  "E__Helping_Hiasat.exe"
+  "E__Helping_Hiasat.exe.manifest"
+  "libE__Helping_Hiasat.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E__Helping_Hiasat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
