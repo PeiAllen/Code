474e9116790da1f8ee466e77dd0c01bad5d5c3dd
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-10-04 23:20:55 -0400

    pushy

--- Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/CMakeFiles/E__Helping_Hiasat.dir/cmake_clean.cmake
+++ Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/CMakeFiles/E__Helping_Hiasat.dir/cmake_clean.cmake
@@ -1,7 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/E__Helping_Hiasat.dir/main.cpp.o"
+  "CMakeFiles/E__Helping_Hiasat.dir/main.cpp.obj"
   "E__Helping_Hiasat.pdb"
-  "E__Helping_Hiasat"
+  "E__Helping_Hiasat.exe"
+  "E__Helping_Hiasat.exe.manifest"
+  "libE__Helping_Hiasat.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
--- Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/cmake_install.cmake
+++ Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /Users/allen/Documents/GitHub/Code/Problems/Codeforces/E. Helping Hiasat
+# Install script for directory: C:/Users/allen/Documents/Code/Problems/Codeforces/E. Helping Hiasat
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/E__Helping_Hiasat")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/allen/Documents/GitHub/Code/Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/allen/Documents/Code/Problems/Codeforces/E. Helping Hiasat/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
