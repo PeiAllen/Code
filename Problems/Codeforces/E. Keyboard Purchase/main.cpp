0f6d7eb3611e23f7bc5537241e0cb1e49ff34205
Allen <peiall@utschools.ca>
2019-10-11 20:15:53 -0400

    pushy

--- Problems/Codeforces/E. Keyboard Purchase/main.cpp
+++ Problems/Codeforces/E. Keyboard Purchase/main.cpp
@@ -17,8 +17,8 @@ void prl(T a, Args... args) {cout<<a<<" ",prl(args...);}
 template<typename T>
 int sz(const T &a){return (int)a.size();}
 #define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
-lli dp[(1<<20)];
-lli am[20][20];
+pair<lli,vector<int>> dp[(1<<20)];
+lli am[21][21];
 int main(){
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
@@ -28,10 +28,37 @@ int main(){
 	sc(s);
 	rep(i,0,n-1){
 		am[s[i]-'a'][s[i+1]-'a']+=1;
+		am[s[i+1]-'a'][s[i]-'a']+=1;
 	}
-	rep(i,0,(1<<m)){
-		int cur=__builtin_popcount(i)-1;
-
+	dp[0]={0,{}};
+	vector<int> te2;
+	rep(i,1,(1<<m)){
+		dp[i]={LLONG_MAX,{}};
+		rep(j,0,m){
+			if(i&(1<<j)){
+				int oth=i^(1<<j);
+				lli te=dp[oth].first;
+				te2.resize(__builtin_popcount(i));
+				te2[0]=j;
+				rep(k,0,sz(dp[oth].second)){
+					te+=am[j][dp[oth].second[k]]*(k+1);
+					te2[k+1]=dp[oth].second[k];
+				}
+				if(te<dp[i].first){
+					dp[i]={te,te2};
+				}
+				te=dp[oth].first;
+				te2[sz(te2)-1]=j;
+				rep(k,sz(dp[oth].second),0){
+					te+=am[j][dp[oth].second[k]]*(sz(dp[oth].second)-k);
+					te2[sz(dp[oth].second)-k-1]=dp[oth].second[k];
+				}
+				if(te<dp[i].first){
+					dp[i]={te,te2};
+				}
+			}
+		}
 	}
+	prl(dp[(1<<m)-1].first);
     return 0;
 }
