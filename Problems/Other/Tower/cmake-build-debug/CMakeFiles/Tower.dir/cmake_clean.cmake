e80f4af9ca0aa5c0f241843be4ee09660ff04892
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-10-21 20:27:43 -0400

    pushy

---
+++ Problems/Other/Tower/cmake-build-debug/CMakeFiles/Tower.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tower.dir/main.cpp.obj"
+  "Tower.pdb"
+  "Tower.exe"
+  "Tower.exe.manifest"
+  "libTower.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tower.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/WAC/Wesley's Anger Contest 2 Problem 3/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_b793b.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_b793b.dir/feature_tests.c.obj"
+  "cmTC_b793b.pdb"
+  "cmTC_b793b.exe"
+  "cmTC_b793b.exe.manifest"
+  "libcmTC_b793b.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_b793b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
