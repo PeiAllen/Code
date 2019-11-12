af8859679fac398657903a81a9c8e312752809b7
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-11-11 21:03:08 -0500

    pushy

---
+++ Problems/Codeforces/B. Unary/cmake-build-debug/CMakeFiles/B__Unary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/B__Unary.dir/main.cpp.obj"
+  "B__Unary.pdb"
+  "B__Unary.exe"
+  "B__Unary.exe.manifest"
+  "libB__Unary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/B__Unary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/battle/cmake-build-debug/CMakeFiles/battle.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/battle.dir/main.cpp.obj"
+  "battle.pdb"
+  "battle.exe"
+  "battle.exe.manifest"
+  "libbattle.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/battle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
