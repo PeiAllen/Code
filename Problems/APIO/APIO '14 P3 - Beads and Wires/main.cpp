ebd55042aa088bd13459f15574df52b48ff3d409
Allen <peiall@utschools.ca>
2019-11-28 09:19:40 -0500

    pushy

--- Problems/APIO/APIO '14 P3 - Beads and Wires/main.cpp
+++ Problems/APIO/APIO '14 P3 - Beads and Wires/main.cpp
@@ -23,20 +23,31 @@ int dp[MAXN][2][2];
 void dfs(int loc, int parent, int w){
 	vector<pii> max11;
 	vector<pii> max10;
+	vector<int> max00;
 	int dp01sum=0;
 	for(pii x:matrix[loc]){
 		if(x.first!=parent){
 			dfs(x.first,loc,x.second);
 			dp01sum+=dp[x.first][0][1];
-			dp[loc][1][1]+=dp[x.first][1][1];
+			dp[loc][1][1]+=dp[x.first][0][1];
 			max11.push_back({-dp[x.first][0][1]+x.second+dp[x.first][1][1],x.first});
 			max10.push_back({-dp[x.first][0][1]+x.second+dp[x.first][1][0],x.first});
+			max00.push_back(-dp[x.first][0][1]+dp[x.first][0][0]);
 		}
 	}
 	sort(max11.begin(),max11.end(),greater<pii>());
 	sort(max10.begin(),max10.end(),greater<pii>());
-	dp[loc][0]=max(dp0sum,(sz(ifrem)&&parent)?w+dp0sum+ifrem[0]:0);
-	dp[loc][1]=max(dp0sum,max(dp1sum,sz(ifrem)>=2?dp0sum+ifrem[0]+ifrem[1]:0));
+	sort(max00.begin(),max00.end(),greater<int>());
+	dp[loc][0][1]=max(dp01sum,sz(max11)?w+dp01sum+max11[0].first:0);
+	dp[loc][1][0]=sz(max00)?dp01sum+max00[0]:0;
+	int toadd=0;
+	if(sz(max00)>=2){
+		int firadd=(max11[0].second==max10[0].second?max11[1].first:max11[0].first);
+		int secadd=(max10[0].second==max11[0].second?max10[1].first:max10[0].first);
+		toadd=max(max10[0].first+firadd,max11[0].first+secadd);
+	}
+	dp[loc][1][0]=max(dp[loc][1][0],dp01sum+toadd);
+	dp[loc][0][0]=max(dp[loc][0][1],max(dp[loc][1][0],sz(max10)?dp01sum+w+max10[0].first:0));
 }
 int main(){
     cin.tie(NULL);
@@ -50,6 +61,6 @@ int main(){
 	    matrix[b].push_back({a,c});
     }
     dfs(1,0,0);
-    prl(dp[1][1]);
+    prl(dp[1][1][0]);
     return 0;
 }
