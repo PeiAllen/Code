3775b1fbecf2784132c33b104c9f2942c0808b62
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2019-12-01 11:36:38 -0500

    pushy

---
+++ Problems/AOC/2019Day1/cmake-build-debug/CMakeFiles/2019Day1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2019Day1.dir/main.cpp.obj"
+  "2019Day1.pdb"
+  "2019Day1.exe"
+  "2019Day1.exe.manifest"
+  "lib2019Day1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2019Day1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
