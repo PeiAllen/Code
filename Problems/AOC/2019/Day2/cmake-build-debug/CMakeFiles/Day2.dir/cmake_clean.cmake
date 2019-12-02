a0002a0fdc4b8c090d13888bb4165312ac84cc6c
Allen <32986987+PeiAllen@users.noreply.github.com>
2019-12-02 00:22:30 -0500

    pushy

---
+++ Problems/AOC/2019/Day2/cmake-build-debug/CMakeFiles/Day2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Day2.dir/main.cpp.obj"
+  "Day2.pdb"
+  "Day2.exe"
+  "Day2.exe.manifest"
+  "libDay2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Day2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
