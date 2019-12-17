7d1ddf86946a6c6fb93cb6f8234791dee54740c5
Allen <peiall@utschools.ca>
2019-12-17 13:14:01 -0500

    pushy

---
+++ Problems/Codeforces/C. Reberland Linguistics/cmake-build-debug/CMakeFiles/C__Reberland_Linguistics.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Reberland_Linguistics.dir/main.cpp.o"
+  "C__Reberland_Linguistics.pdb"
+  "C__Reberland_Linguistics"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Reberland_Linguistics.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
