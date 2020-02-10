9f73c40bdfc057145ec32722af92ce1052fbee34
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2020-02-09 19:03:31 -0500

    pushy

---
+++ Problems/Codeforces/#618Div2/cmake-build-debug/CMakeFiles/_618Div2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/_618Div2.dir/main.cpp.obj"
+  "_618Div2.pdb"
+  "_618Div2.exe"
+  "_618Div2.exe.manifest"
+  "lib_618Div2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/_618Div2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
