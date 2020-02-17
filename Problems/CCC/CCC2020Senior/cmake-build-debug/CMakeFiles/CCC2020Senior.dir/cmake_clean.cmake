dce29929d795638bb0512937aade1d6ddb583898
Allen <peiall@utschools.ca>
2020-02-16 22:15:55 -0500

    pushy

---
+++ Problems/CCC/CCC2020Senior/cmake-build-debug/CMakeFiles/CCC2020Senior.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CCC2020Senior.dir/main.cpp.o"
+  "CCC2020Senior.pdb"
+  "CCC2020Senior"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CCC2020Senior.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
