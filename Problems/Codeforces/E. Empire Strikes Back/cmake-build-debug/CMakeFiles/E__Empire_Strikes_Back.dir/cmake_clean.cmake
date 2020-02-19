5b15289f0a46beeadb71a8b765bb3aed660bb91f
Allen <32986987+PeiAllen@users.noreply.github.com>
2020-02-18 19:54:36 -0500

    pushy

---
+++ Problems/Codeforces/E. Empire Strikes Back/cmake-build-debug/CMakeFiles/E__Empire_Strikes_Back.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E__Empire_Strikes_Back.dir/main.cpp.obj"
+  "E__Empire_Strikes_Back.pdb"
+  "E__Empire_Strikes_Back.exe"
+  "E__Empire_Strikes_Back.exe.manifest"
+  "libE__Empire_Strikes_Back.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E__Empire_Strikes_Back.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Problems/Codeforces/E. Empire Strikes Back/cmake-build-debug/CMakeFiles/E__Empire_Strikes_Back.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for E__Empire_Strikes_Back.
+# This may be replaced when dependencies are built.
