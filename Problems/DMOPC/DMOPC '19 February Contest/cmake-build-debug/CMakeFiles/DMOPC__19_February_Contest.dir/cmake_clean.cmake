40cde5728eb1e63596fd8ae26aa21a97691b7b89
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2020-02-28 20:14:57 -0500

    pushy

---
+++ Problems/DMOPC/DMOPC '19 February Contest/cmake-build-debug/CMakeFiles/DMOPC__19_February_Contest.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DMOPC__19_February_Contest.dir/main.cpp.obj"
+  "DMOPC__19_February_Contest.pdb"
+  "DMOPC__19_February_Contest.exe"
+  "DMOPC__19_February_Contest.exe.manifest"
+  "libDMOPC__19_February_Contest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DMOPC__19_February_Contest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
