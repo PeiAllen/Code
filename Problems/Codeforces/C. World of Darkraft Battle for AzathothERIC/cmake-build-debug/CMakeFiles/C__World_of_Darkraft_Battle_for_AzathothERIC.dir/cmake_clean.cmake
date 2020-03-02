7a50596005b70304bd5047c1f85472483ceff198
Allen <32986987+PeiAllen@users.noreply.github.com>
2020-03-01 23:05:34 -0500

    pushy

---
+++ Problems/Codeforces/C. World of Darkraft Battle for AzathothERIC/cmake-build-debug/CMakeFiles/C__World_of_Darkraft_Battle_for_AzathothERIC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__World_of_Darkraft_Battle_for_AzathothERIC.dir/main.cpp.obj"
+  "C__World_of_Darkraft_Battle_for_AzathothERIC.pdb"
+  "C__World_of_Darkraft_Battle_for_AzathothERIC.exe"
+  "C__World_of_Darkraft_Battle_for_AzathothERIC.exe.manifest"
+  "libC__World_of_Darkraft_Battle_for_AzathothERIC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__World_of_Darkraft_Battle_for_AzathothERIC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
