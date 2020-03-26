c10998e09b89dd4cb98370e25867468078793ea7
Allen <32986987+PeiAllen@users.noreply.github.com>
2020-03-25 20:07:30 -0400

    pushy

---
+++ Problems/Codeforces/E. Decypher the String/cmake-build-debug/CMakeFiles/E__Decypher_the_String.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E__Decypher_the_String.dir/main.cpp.obj"
+  "E__Decypher_the_String.pdb"
+  "E__Decypher_the_String.exe"
+  "E__Decypher_the_String.exe.manifest"
+  "libE__Decypher_the_String.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/E__Decypher_the_String.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Problems/Codeforces/Virtual/main.cpp
+++ Problems/Codeforces/Virtual/main.cpp
@@ -5,110 +5,138 @@ typedef pair<int,int> pii;
 typedef pair<lli,lli> pll;
 template<typename T>
 int sz(const T &a){return (int)a.size();}
-const int MAXN=2e5+1;
-pll dom[MAXN];
-lli ans[MAXN];
-pair<pii,int> que[MAXN];
-struct node{
-    lli data,lazy;
-    bool erase;
-    int le,ri;
-    node* left,*right;
-    node(){
-        data=0,lazy=0,erase=0,le=0,ri=0,left=NULL,right=NULL;
-    }
-    int mid(){return (le+ri)/2;}
-    void mt(int l, int r){
-        le=l,ri=r;
-        if(le!=ri){
-            left=new node();
-            right=new node();
-            left->mt(le,mid()),right->mt(mid()+1,ri);
+const int MAXN=1e5+1;
+lli arr[MAXN];
+struct seg{
+    struct node {
+        lli data, sum[10];
+        pair<vector<int>, int> inu[10];
+        node() {
+            data = 0;
+            for (int i = 0; i < 10; i++)sum[i] = 0, inu[i] = {{}, -1};
         }
+    }t[2*MAXN];
+    void pushup(int ind, int le, int ri){
+        int mid=(le+ri)/2;
+        int left=ind+1,right=ind+(mid-le+1)*2;
+        t[ind].data=t[left].data+t[right].data;
+        for(int i=0;i<10;i++)t[ind].sum[i]=t[left].sum[i]+t[right].sum[i];
     }
-    void rl(){
-        if(erase)data=0;
-        data+=lazy;
+    void mt(int ind, int le, int ri){
+        int mid=(le+ri)/2;
+        int left=ind+1,right=ind+(mid-le+1)*2;
         if(le!=ri){
-            if(erase){
-                left->lazy=0,right->lazy=0;
+            mt(left,le,mid),mt(right,mid+1,ri);
+            pushup(ind,le,ri);
+        }
+        else{
+            t[ind].data=arr[le];
+            lli cur=1;
+            while(t[ind].data){
+                t[ind].sum[t[ind].data%10]+=cur;
+                cur*=10;
+                t[ind].data/=10;
             }
-            left->lazy+=lazy;
-            left->erase|=erase;
-            right->lazy+=lazy;
-            right->erase|=erase;
+            t[ind].data=arr[le];
         }
-        erase=0,lazy=0;
     }
-    void update(int l, int r, lli val, bool er){
-        rl();
-        if(l>ri||r<le)return;
+    void merge(int ind, const vector<int>& a, int b, int & cnt){
+        if(t[ind].inu[b].second!=-1){
+            if(t[ind].inu[b].second!=b){
+                int oldb=b;
+                b=t[ind].inu[b].second;
+                t[ind].inu[oldb]={{},-1};
+            }
+        }
+        for(int x:a) {
+            cnt++;
+            if(t[ind].inu[x].second!=-1){
+                if(t[ind].inu[x].second==x){
+                    for(int y:t[ind].inu[x].first){
+                        t[ind].inu[y]={{},b};
+                        t[ind].inu[b].second=b;
+                        t[ind].inu[b].first.push_back(y);
+                        cnt++;
+                    }
+                    t[ind].inu[x]={{},-1};
+                    continue;
+                }
+                else{
+                    continue;
+                }
+            }
+            t[ind].inu[b].second=b;
+            t[ind].inu[b].first.push_back(x);
+            t[ind].inu[x]={{},b};
+        }
+    }
+    void rl(int ind, int le, int ri){
+        int mid=(le+ri)/2;
+        int left=ind+1,right=ind+(mid-le+1)*2;
+        int cnt=0;
+        for(int i=0;i<10;i++){
+            if(t[ind].inu[i].second==i) {
+                for(lli x:t[ind].inu[i].first) {
+                    cnt++;
+                    t[ind].data += (t[ind].inu[i].second-x) * t[ind].sum[x];
+                    t[ind].sum[t[ind].inu[i].second] += t[ind].sum[x];
+                    t[ind].sum[x]=0;
+                }
+            }
+        }
+        if(le!=ri){
+            for(int i=0;i<10;i++){
+                if(t[ind].inu[i].second==i){
+                    merge(left,t[ind].inu[i].first,t[ind].inu[i].second,cnt);
+                    merge(right,t[ind].inu[i].first,t[ind].inu[i].second,cnt);
+                }
+            }
+        }
+        assert(cnt<=40);
+        for(int i=0;i<10;i++)t[ind].inu[i]={{},-1};
+    }
+    void update(int ind, int le, int ri, int l, int r, int a, int b){
+        int mid=(le+ri)/2;
+        int left=ind+1,right=ind+(mid-le+1)*2;
+        rl(ind,le,ri);
+        if(r<le||l>ri)return;
         if(le>=l&&ri<=r){
-            lazy=val;
-            erase=er;
-            rl();
+            t[ind].inu[a]={{},b},t[ind].inu[b]={{a},b};
+            rl(ind,le,ri);
             return;
         }
-        left->update(l,r,val,er),right->update(l,r,val,er);
+        update(left,le,mid,l,r,a,b),update(right,mid+1,ri,l,r,a,b);
+        pushup(ind,le,ri);
     }
-    lli query(int loc){
-        rl();
-        if(loc>ri||loc<le)return (lli)0;
-        if(le==ri){
-            return data;
+    lli query(int ind, int le, int ri, int l, int r){
+        int mid=(le+ri)/2;
+        int left=ind+1,right=ind+(mid-le+1)*2;
+        rl(ind,le,ri);
+        if(r<le||l>ri)return 0;
+        if(le>=l&&ri<=r){
+            return t[ind].data;
         }
-        return left->query(loc)+right->query(loc);
+        return query(left,le,mid,l,r)+query(right,mid+1,ri,l,r);
     }
-};
+}tree;
 int main(){
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int n;
-    cin>>n;
+    int n,q;
+    cin>>n>>q;
     for(int i=1;i<=n;i++){
-        cin>>dom[i].first>>dom[i].second;
+        cin>>arr[i];
     }
-    int q;
-    cin>>q;
-    for(int i=0;i<q;i++){
-        cin>>que[i].first.first>>que[i].first.second;
-        if(n==1000&&i==1){
-            printf("%d %d\n",que[i].first.first,que[i].first.second);
-            return 0;
+    tree.mt(1,1,n);
+    int a,b,c,d,e;
+    while(q--){
+        cin>>a>>b>>c;
+        if(a==1){
+            cin>>d>>e;
+            if(d==e)continue;
+            tree.update(1,1,n,b,c,d,e);
         }
-        que[i].second=i;
+        else printf("%lli\n",tree.query(1,1,n,b,c));
     }
-    sort(que,que+q,greater<pair<pii,int>>());
-    int l=n;
-    node tree;
-    tree.mt(1,n);
-    for(int i=0;i<q;i++){
-        while(l>que[i].first.first){
-            l--;
-            int rightmost=(upper_bound(dom+1+l,dom+n+1,dom[l].first+dom[l].second,[&](auto val, auto it){
-                return val<it.first;
-            })-dom)-1;
-            assert(dom[l].first+dom[l].second>=dom[rightmost].first);
-            assert(rightmost>=l&&rightmost<=n);
-            if(rightmost==l){
-                tree.update(l+1,n,dom[l+1].first-(dom[l].first+dom[l].second),0);
-            }
-            else {
-                if(rightmost<n){
-                    lli oldval=tree.query(rightmost+1)-tree.query(rightmost);
-                    assert(oldval>=0);
-                    assert(dom[rightmost+1].first-(dom[l].first+dom[l].second)>0);
-                    if(oldval>dom[rightmost+1].first-(dom[l].first+dom[l].second)){
-                        tree.update(rightmost+1,n,dom[rightmost+1].first-(dom[l].first+dom[l].second)-oldval,0);
-                    }
-                }
-                tree.update(l + 1, rightmost, 0, 1);
-            }
-        }
-        assert(l==que[i].first.first);
-        ans[que[i].second]=tree.query(que[i].first.second);
-        assert(ans[que[i].second]>=0);
-    }
-    for(int i=0;i<q;i++)printf("%lli\n",ans[i]);
     return 0;
 }
