c5cfbc8c1daa39a9115b459934349af23f00a5b8
Allen <peiall@utschools.ca>
2021-10-12 14:29:58 -0400

    pushy

---
+++ Problems/Other/macvscode/main.cpp
@@ -0,0 +1,17 @@
+#include "bits/stdc++.h"
+using namespace std;
+using pii = pair<int, int>;
+// __int128 x;
+
+int main() {
+    int n;
+    vector<int> arr(100, 32);
+    // vector<pii> arr3 = {{2, 3}, {1, 5}};
+    // map<pii, pii> arr2 = {{{2, 3}, {1, 2}}, {{2, 5}, {1, 3}}};
+    // map<int, pii> arr5 = {{1, {2, 3}}};
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        // printf("%d\n", __gcd(i, int(x)));
+        // x = 3;
+    }
+}
