896b244ba75d5528f7816ff55a84d91e27ab8f10
Allen <peiall@utschools.ca>
2021-10-31 20:57:58 -0400

    pushy

--- Problems/Contest/main.cpp
+++ Problems/Contest/main.cpp
@@ -6,93 +6,40 @@ using pll = pair<ll,ll>;
 #define A first
 #define B second
 template<typename T> int sz(const T &a){return int(a.size());}
-const ll mod=998244352;
-const int MN=3e3+1;
-pii dsu[MN];
-int find(int a){
-    if(dsu[a].first==a)return a;
-    return dsu[a].first=find(dsu[a].first);
-}
-bool uni(int a, int b){
-    int ap=find(a),bp=find(b);
-    if(ap==bp)return false;
-    if(dsu[ap].second<dsu[bp].second)swap(ap,bp);
-    dsu[bp].first=ap;
-    dsu[ap].second+=dsu[bp].second;
-    return true;
-}
-vector<int> adj[MN];
-vector<pii> edges;
-ll dp[(1<<21)];
-pii to[(1<<21)][21];
-ll fact[MN];
-int n;
-ll solve(int a){
-    if(__builtin_popcount(a)<=1)return 1;
-    if(dp[a]!=-1)return dp[a];
-    ll ans=0;
-    for(int i=0;i<n-1;i++){
-        if(a&(1<<i)){
-            ans=(ans+solve(to[a][i].first)*solve(to[a][i].second)%mod)%mod;
-        }
-    }
-    return dp[a]=ans;
-}
+const int MN=1e5+1;
+const ll MV=2e9;
+ll gc[MN+2];
+ll gc2[MN+2];
 int main(){
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int k;
-    cin>>n>>k;
-    k=n-1-k;
-    edges.resize(n-1);
-    fact[0]=1;
-    for(int i=1;i<=n-1;i++)if(i>0)fact[i]=fact[i-1]*ll(i)%mod;
-    for(int i=0;i<n-1;i++){
-        cin>>edges[i].first>>edges[i].second;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        ll a;
+        cin>>n;
+        gc[1]=1;
+        for(int i=2;i<=n+1;i++){
+            if(gc[i-1]>MV)gc[i]=gc[i-1];
+            gc[i]=gc[i-1]*ll(i)/__gcd(gc[i-1],ll(i));
+            if(gc2[i-1]>MV)gc2[i]=gc2[i-1];
+            else gc2[i]=gc2[i-1]*ll(i)/__gcd(gc2[i-1],ll(i));
+            if(gc2[i]>=MV)assert(gc[i]>MV||gc[i]<-MV);
+        }
+        bool work=true;
+//        for(int i=1;i<=n;i++){
+//            cin>>a;
+//            int lo=1,hi=n+1;
+//            while(lo!=hi){
+//                int mid=(lo+hi+1)/2;
+//                if(a%gc[mid]==0)lo=mid;
+//                else hi=mid-1;
+//            }
+//            if(lo>i)work=false;
+//        }
+//        if(work)printf("YES\n");
+//        else printf("NO\n");
     }
-    ll ans=0;
-    for(int i=0;i<(1<<(n-1));i++){
-        dp[i]=-1;
-        for(int j=0;j<n-1;j++){
-            if(i&(1<<j)){
-                for(int l=1;l<=n;l++)dsu[l]={l,1};
-                for(int l=0;l<n-1;l++){
-                    if(i&(1<<l)){
-                        if(l!=j){
-                            uni(edges[l].first,edges[l].second);
-                        }
-                    }
-                }
-                int left=0,right=0;
-                for(int l=0;l<n-1;l++){
-                    if(i&(1<<l)) {
-                        if (l == j)continue;
-                        if (find(edges[l].first) == find(edges[l].second) && find(edges[l].first) == find(edges[j].first))left += (1 << l);
-                        else if (find(edges[l].first) == find(edges[l].second) && find(edges[l].first) == find(edges[j].second))right += (1 << l);
-                    }
-                }
-                to[i][j]={left,right};
-            }
-        }
-    }
-    for(int i=0;i<(1<<(n-1));i++){
-        if(__builtin_popcount(i)!=k)continue;
-        for(int j=1;j<=n;j++)dsu[j]={j,1};
-        for(int j=0;j<n-1;j++){
-            if(!(i&(1<<j))){
-                uni(edges[j].first,edges[j].second);
-            }
-        }
-        ll cur=1;
-        map<int,int> teedges;
-        for(int l=0;l<n-1;l++){
-            if(!(i&(1<<l)))teedges[find(edges[l].first)]+=(1<<l);
-        }
-        for(auto x:teedges){
-            cur=cur*solve(x.second)%mod;
-        }
-        ans=(ans+cur)%mod;
-    }
-    printf("%lli\n",ans);
     return 0;
 }
