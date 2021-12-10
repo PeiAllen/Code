d33e98dcdbebe0e3f7c6fa8307885abb941ef106
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2021-12-10 16:40:50 -0500

    pushy

---
+++ Problems/AOC/2021Day5/cmake-build-debug/CMakeFiles/2021Day5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "2021Day5.exe"
+  "2021Day5.exe.manifest"
+  "2021Day5.pdb"
+  "CMakeFiles/2021Day5.dir/main.cpp.obj"
+  "lib2021Day5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2021Day5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
