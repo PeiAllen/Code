1dcb4cc6c83d4957e793c4fa81ed2babbe66bb64
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2021-12-17 17:20:45 -0500

    pushy

---
+++ Problems/COCI/COCI '21 Contest 2 #5 Osumnjičeni/cmake-build-debug/CMakeFiles/COCI__21_Contest_2__5_Osumnjieni.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COCI__21_Contest_2__5_Osumnjieni.dir/main.cpp.obj"
+  "COCI__21_Contest_2__5_Osumnjieni.exe"
+  "COCI__21_Contest_2__5_Osumnjieni.exe.manifest"
+  "COCI__21_Contest_2__5_Osumnjieni.pdb"
+  "libCOCI__21_Contest_2__5_Osumnjieni.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COCI__21_Contest_2__5_Osumnjieni.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
