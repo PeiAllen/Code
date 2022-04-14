38225d3cdcd05d1522872aca9e8e742ce38bdd4f
Allen <peiall@utschools.ca>
2022-04-14 12:30:54 -0400

    pushy

---
+++ Problems/CSES/Weird Algorithm/cmake-build-debug/CMakeFiles/Weird_Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Weird_Algorithm.dir/main.cpp.o"
+  "Weird_Algorithm.pdb"
+  "Weird_Algorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Weird_Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
