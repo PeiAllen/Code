557b3770629fd36dd40de57dadefd105ec1b6e08
Allen <peiall@utschools.ca>
2022-04-24 20:13:48 -0400

    pushy

--- Problems/Contest/brute.cpp
+++ Problems/Contest/brute.cpp
@@ -1,37 +1,125 @@
-#include "bits/stdc++.h"
+#include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-using pii = pair<int,int>;
-using pll = pair<ll,ll>;
-template<typename T> int sz(const T &a){return int(a.size());}
-const int MN=1e6+1;
-int arr[MN];
-int poss[MN];
-int main(){
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    int n;
-    cin>>n;
-    vector<int> vals(n);
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-        vals[i]=i+1;
+#define fp(i,l,r) for(register int (i)=(l);(i)<=(r);++(i))
+#define fd(i,l,r) for(register int (i)=(l);(i)>=(r);--(i))
+#define fe(i,u) for(register int (i)=front[(u)];(i);(i)=e[(i)].next)
+#define mem(a) memset((a),0,sizeof (a))
+#define O(x) cerr<<#x<<':'<<x<<endl
+inline int read(){
+    int x=0,f=1;char ch=getchar();
+    while(!isdigit(ch)){if(ch=='-')f=-1;ch=getchar();}
+    while(isdigit(ch))x=x*10+ch-'0',ch=getchar();
+    return x*f;
+}
+void wr(int x){
+    if(x<0)putchar('-'),x=-x;
+    if(x>=10)wr(x/10);
+    putchar(x%10+'0');
+}
+const int MAXN=5e5+10,MAXM=5e4+10,inf=2e8;
+struct qry{
+    int op,x,y,w;
+}a[500];
+int m,T,lim[MAXM],L[MAXM],R[MAXM],w[MAXN],c[MAXN],pp,bel[MAXN],buc[13],cnt,top;
+bool vis[MAXN],used[MAXN];
+struct solver{
+    int a1[MAXN],a3[MAXN],b1[MAXN],b2[MAXN],la1,la3,lb1,lb2;
+    inline void init(){la1=la3=lb1=lb2=0;}
+    inline void ins(int w,int c){
+        if(w==1)a1[++la1]=c;else if(w==2)b1[++lb1]=c;else a3[++la3]=c;
     }
-    do{
-        bool work=true;
-        for(int i=0;i<n&&work;i++){
-            if(min(vals[(i-1+n)%n],vals[(i+1)%n])!=arr[i])work=false;
+    inline void work(){
+        lb2=lb1;fp(i,1,lb2)b2[i]=b1[i];
+        sort(a1+1,a1+1+la1);sort(a3+1,a3+1+la3);
+        for(int i=1;i<la1;i+=2)b1[++lb1]=a1[i]+a1[i+1];sort(b1+1,b1+1+lb1);
+        for(int i=2;i<la1;i+=2)b2[++lb2]=a1[i]+a1[i+1];sort(b2+1,b2+1+lb2);
+        fp(i,1,lb1)b1[i]+=b1[i-1];
+        b2[0]=la1?a1[1]:inf;fp(i,1,lb2)b2[i]+=b2[i-1];
+    }
+    inline int gb1(int x){return x>lb1?inf:b1[x];}
+    inline int gb2(int x){return x>lb2?inf:b2[x];}
+    inline int ask1(int x){
+        if(x<=0)return 0;
+        return (x&1)?min(gb2(x/2),gb1((x+1)/2)):gb1(x/2);
+    }
+    inline int ask(int x){
+        if(x<=0)return 0;
+        int t=0,res=ask1(x);
+        fp(i,1,la3)t+=a3[i],x-=3,res=min(res,t+ask1(x));
+        return res;
+    }
+}S;
+int ans=inf,dp[300],g[20][300],tmp2[300],len[MAXM],b[20],tot,b1[20],tot1,id[MAXM],tmp[300],dp2[300];
+inline void merge(int a[],int b[]){
+    fp(i,0,T)tmp2[i]=inf;
+    fp(i,0,T)fp(j,0,T-i)tmp2[i+j]=min(tmp2[i+j],a[i]+b[j]);
+    fp(i,0,T)a[i]=tmp2[i];
+}
+inline void solve(){
+    fp(i,1,pp)if(a[i].op==3){
+            if(used[a[i].x]&&used[a[i].y])return;
         }
-        if(work){
-            for(int i=0;i<n;i++){
-                if(poss[i]==0)poss[i]=vals[i];
-                else if(poss[i]!=vals[i])poss[i]=INT_MAX;
+    fp(i,0,T)dp2[i]=dp[i];
+    fp(i,1,tot1){
+        int x=b1[i],s=0,s2=0;
+        fp(j,1,cnt)if(bel[buc[j]]==x){
+                s+=w[buc[j]];s2+=c[buc[j]];
             }
-        }
-    }while(next_permutation(vals.begin(),vals.end()));
-    for(int i=0;i<n;i++){
-        if(poss[i]!=INT_MAX)printf("%d%c",poss[i]," \n"[i==n-1]);
-        else printf("0%c"," \n"[i==n-1]);
+        fp(j,-s,T-s)tmp[j+s]=g[i][j+len[x]]+s2;
+        merge(dp2,tmp);
     }
+    int res=dp2[T];
+    fp(i,1,pp){
+        if(a[i].op==1){
+            if(used[a[i].x]&&used[a[i].y])res-=a[i].w;
+        }
+        else if(a[i].op==2){
+            if(used[a[i].x]&&used[a[i].y])res+=a[i].w;
+        }
+    }
+    ans=min(ans,res);
+}
+void dfs(int d){
+    if(d==tot+1){solve();return;}
+    dfs(d+1);
+    buc[++cnt]=b[d];used[b[d]]=1;dfs(d+1);
+    used[b[d]]=0;--cnt;
+}
+main(){
+    m=read();T=read();
+    fp(i,1,m){
+        int num=read();lim[i]=read();T-=lim[i];
+        L[i]=R[i-1]+1;R[i]=L[i]+num-1;
+        fp(j,L[i],R[i])w[j]=read(),c[j]=read(),bel[j]=i;
+    }
+    T=max(T,0);
+    pp=read();
+    fp(i,1,pp){
+        a[i].op=read();
+        int x1=read(),y1=read(),x2=read(),y2=read();
+        a[i].x=L[x1]+y1-1,a[i].y=L[x2]+y2-1;
+        if(!vis[a[i].x])b[++tot]=a[i].x;vis[a[i].x]=1;
+        if(!vis[a[i].y])b[++tot]=a[i].y;vis[a[i].y]=1;
+        if(a[i].op!=3)a[i].w=read();
+    }
+    fp(i,1,T)dp[i]=inf;
+    fp(i,1,m){
+        S.init();
+        fp(j,L[i],R[i]){
+            if(!vis[j])S.ins(w[j],c[j]);
+            else len[i]+=w[j];
+        }
+        S.work();
+        if(len[i])b1[++tot1]=i,id[i]=tot1;
+        if(len[i]){
+            fp(j,-len[i],T)g[tot1][j+len[i]]=S.ask(lim[i]+j);
+        }
+        else{
+            fp(j,0,T)tmp[j]=S.ask(lim[i]+j);
+            merge(dp,tmp);
+        }
+    }
+    dfs(1);
+    printf("%d\n",ans>=110000000?-1:ans);
     return 0;
 }
--- Problems/Contest/main.cpp
+++ Problems/Contest/main.cpp
@@ -4,99 +4,202 @@ using ll = long long;
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
 template<typename T> int sz(const T &a){return int(a.size());}
-const int MN=5e5+1;
-int arr[MN];
-bool solve(int n){
-    vector<pii> intervals;
-    set<int> vals;
-    for(int i=1;i<=n;i++)vals.insert(i);
-    int last=-1;
-    for(int i=0;i<n;i++){
-        if(arr[i]>0){
-            for(int j=last+1;j<i;j++){
-                intervals.push_back({min((last==-1?0:arr[last]),arr[i])+1,max((last==-1?0:arr[last]),arr[i])-1});
+const int MN=5e4+1;
+const int MM=2e6+1;
+const int MD=41;
+const int MV=1e9;
+vector<pii> items[MN];
+vector<bool> spec[MN];
+vector<int> badind[MN];
+int needed[MN];
+int am[MN];
+bool bad[MN];
+int cost[MD];
+int notneeded[MN];
+int costspec[13][MD+13*201];
+int anscost[MD];
+int dp[MM];
+int dp2[MN];
+pair<pii,pair<pii,pii>> relation[MN];
+vector<int> solve(vector<pii> vals, int l, int r){
+    int masum=0;
+    vector<int> ones,twos,threes;
+    for(auto x:vals){
+        if(x.first==0)continue;
+        masum+=x.first;
+        if(x.first==1)ones.push_back(x.second);
+        else if(x.first==2)twos.push_back(x.second);
+        else threes.push_back(x.second);
+    }
+    sort(ones.begin(),ones.end());
+    sort(twos.begin(),twos.end());
+    sort(threes.begin(),threes.end());
+    for(int i=1;i<sz(ones);i++)ones[i]+=ones[i-1];
+    for(int i=1;i<sz(twos);i++)twos[i]+=twos[i-1];
+    for(int i=1;i<sz(threes);i++)threes[i]+=threes[i-1];
+    for(int i=0;i<=masum;i++){
+        int lo=max(0,(i-sz(ones)+1)/2),hi=min(i/2,sz(twos));
+        int lmin=lo,rmax=hi;
+        auto getcost=[&](int a){
+            if(a<lmin||a>rmax)return MV;
+            return (a==0?0:twos[a-1])+(i-a*2==0?0:ones[i-a*2-1]);
+        };
+        if(lo>hi)dp[i]=INT_MAX;
+        else{
+            while(lo!=hi){
+                int mid=(lo+hi)/2;
+                int slope=getcost(mid+1)-getcost(mid);
+                if(slope<=0)lo=mid+1;
+                else hi=mid;
             }
-            vals.erase(arr[i]);
-            last=i;
+            dp[i]=getcost(lo);
         }
     }
-    for(int j=last+1;j<n;j++){
-        intervals.push_back({min((last==-1?0:arr[last]),0)+1,max((last==-1?0:arr[last]),0)-1});
+    int mareached[2]={(sz(ones)+2*sz(twos))/2*2,(sz(ones)==0?-1:((sz(ones)+2*sz(twos))%2==1?sz(ones)+2*sz(twos):sz(ones)+2*sz(twos)-1))};
+    for(int i=0;i<=masum;i++){
+        int rmax=min(i/3,sz(threes));
+        dp2[i]=INT_MAX;
+        int lo=(max(0,(i-mareached[i%2]+2)/3)+1)/2,hi=rmax/2;//solve eventhrees
+        int lmin=lo;
+        auto getcost=[&](int a){
+            if(a<lmin||a>rmax)return MV;
+            return (a==0?0:threes[a-1])+dp[i-3*a];
+        };
+        if(lo<=hi){
+            while(lo!=hi){
+                int mid=(lo+hi)/2;
+                int slope=getcost(2*(mid+1))-getcost(2*mid);
+                if(slope<=0)lo=mid+1;
+                else hi=mid;
+            }
+            dp2[i]=min(dp2[i],getcost(2*lo));
+        }
+        lo=max(0,(i-mareached[(i+1)%2]+2)/3)/2,hi=(rmax-1)/2;//solve oddthrees
+        lmin=lo;
+        if(rmax==0)hi=-1;
+        if(lo<=hi){
+            while(lo!=hi){
+                int mid=(lo+hi)/2;
+                int slope=getcost(2*(mid+1)+1)-getcost(2*mid+1);
+                if(slope<=0)lo=mid+1;
+                else hi=mid;
+            }
+            dp2[i]=min(dp2[i],getcost(2*lo+1));
+        }
     }
-    sort(intervals.begin(),intervals.end(),[&](const auto &lhs, const auto &rhs){
-        return lhs.second<rhs.second;
-    });
-    for(auto x:intervals){
-        auto it=vals.lower_bound(x.first);
-        if(it==vals.end()||*it>x.second)return false;
-        vals.erase(it);
+    vector<int> toret(r-l+1);
+    for(int i=l;i<=r;i++){
+        toret[i-l]=(i>masum?INT_MAX:dp2[i]);
     }
-    printf("YES\n");
-    return true;
+    for(int i=r+1;i<=masum;i++){
+        toret[r-l]=min(toret[r-l],dp2[i]);
+    }
+    return toret;
 }
 int main(){
     cin.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int n,q,a,b;
-    cin>>n>>q;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    int m,t;
+    cin>>m>>t;
+    for(int i=1;i<=m;i++){
+        int n;
+        cin>>n>>needed[i];
+        am[i]=n;
+        t-=needed[i];
+        items[i].resize(n+1);
+        spec[i].resize(n+1);
+        badind[i].resize(n+1);
+        for(int j=1;j<=n;j++)cin>>items[i][j].first>>items[i][j].second;
     }
-    for(int tc=0;tc<=q;tc++) {
-        if(tc>0) {
-            cin >> a >> b;
-            arr[a - 1] = b;
-        }
-        pii largest={0,-1};
-        for(int i=0;i<n;i++) {
-            if(arr[i]>0)largest = max(largest, {arr[i], i});
-        }
-        int last = INT_MAX;
-        bool done=false;
-        for (int i = largest.second; i >= 0; i--) {
-            if (arr[i] > 0) {
-                if (arr[i] >= last) {
-                    printf("NO\n");
-                    done=true;
-                    break;
+    for(int i=1;i<=t;i++)cost[i]=INT_MAX;
+    int p;
+    cin>>p;
+    for(int i=0;i<p;i++){
+        cin>>relation[i].first.first>>relation[i].second.first.first>>relation[i].second.first.second>>relation[i].second.second.first>>relation[i].second.second.second;
+        if(relation[i].first.first<=2)cin>>relation[i].first.second;
+        spec[relation[i].second.first.first][relation[i].second.first.second]=true;
+        spec[relation[i].second.second.first][relation[i].second.second.second]=true;
+        bad[relation[i].second.first.first]=true;
+        bad[relation[i].second.second.first]=true;
+    }
+    vector<int> specind;
+    vector<pii> baditems;
+    for(int i=1;i<=m;i++){
+        if(!bad[i]){
+            vector<int> te=solve(items[i],needed[i],needed[i]+t);
+            vector<int> teres(t+1,INT_MAX);
+            for(int a=0;a<=t;a++){
+                if(te[a]!=INT_MAX)for(int b=0;b<=t;b++){
+                    if(cost[b]!=INT_MAX)teres[min(a+b,t)]=min(teres[min(a+b,t)],te[a]+cost[b]);
                 }
-                last = arr[i];
             }
+            for(int j=0;j<=t;j++)cost[j]=teres[j];
         }
-        if(done)continue;
-        last = INT_MAX;
-        for (int i = largest.second; i < n; i++) {
-            if (arr[i] > 0) {
-                if (arr[i] >= last) {
-                    printf("NO\n");
-                    done=true;
-                    break;
-                }
-                last = arr[i];
+        else{
+            vector<pii> temp;
+            for(int j=1;j<=am[i];j++){
+                if(spec[i][j])notneeded[i]+=items[i][j].first,badind[i][j]=sz(baditems),baditems.push_back({i,j});
+                else temp.push_back(items[i][j]);
             }
-        }
-        if(done)continue;
-        if (largest.first == n) {
-            if (!solve(n))printf("NO\n");
-        } else {
-            if (largest.second != n - 1 && arr[largest.second + 1] == 0) {
-                arr[largest.second + 1] = n;
-                if (solve(n)) {
-                    arr[largest.second + 1] = 0;
-                    continue;
-                }
-                arr[largest.second + 1] = 0;
+            int micalc=max(0,needed[i]-notneeded[i]);
+            vector<int> te=solve(temp,micalc,needed[i]+t);
+            for(int j=micalc;j<=needed[i]+t;j++){
+                costspec[sz(specind)][j-micalc]=te[j-micalc];
             }
-            if (largest.second != 0 && arr[largest.second - 1] == 0) {
-                arr[largest.second - 1] = n;
-                if (solve(n)) {
-                    arr[largest.second - 1] = 0;
-                    continue;
-                }
-                arr[largest.second - 1] = 0;
-            }
-            printf("NO\n");
+            specind.push_back(i);
         }
     }
+    int baditemcnt=sz(baditems);
+    int ans=INT_MAX;
+    for(int i=0;i<(1<<baditemcnt);i++){
+        int fixedcost=0;
+        bool work=true;
+        for(int j=0;j<p;j++){
+            int a=badind[relation[j].second.first.first][relation[j].second.first.second];
+            int b=badind[relation[j].second.second.first][relation[j].second.second.second];
+            if((i&(1<<a))&&(i&(1<<b))){
+                if(relation[j].first.first==3)work=false;
+                else if(relation[j].first.first==2)fixedcost+=relation[j].first.second;
+                else fixedcost-=relation[j].first.second;
+            }
+        }
+        if(!work)continue;
+        for(int j=0;j<baditemcnt;j++){
+            if(i&(1<<j)){
+                fixedcost+=items[baditems[j].first][baditems[j].second].second;
+            }
+        }
+        for(int j=0;j<=t;j++)anscost[j]=cost[j];
+        int curspec=0;
+        for(auto x:specind){
+            int micalc=max(0,needed[x]-notneeded[x]);
+            int given=0;
+            for(int j=0;j<baditemcnt;j++){
+                if(i&(1<<j)){
+                    if(baditems[j].first==x){
+                        given+=items[x][baditems[j].second].first;
+                    }
+                }
+            }
+            vector<int> teres(t+1,INT_MAX);
+            for(int a=0;a<t;a++){
+                if(a+needed[x]-given-micalc>=0&&costspec[curspec][a+needed[x]-given-micalc]!=INT_MAX)for(int b=0;b<=t;b++){
+                    if(anscost[b]!=INT_MAX)teres[min(a+b,t)]=min(teres[min(a+b,t)],costspec[curspec][a+needed[x]-given-micalc]+anscost[b]);
+                }
+            }
+            int best=INT_MAX;
+            for(int j=max(0,t+needed[x]-given-micalc);j<=needed[x]+t-micalc;j++){
+                best=min(best,costspec[curspec][j]);
+            }
+            if(best!=INT_MAX)for(int b=0;b<=t;b++){
+                if(anscost[b]!=INT_MAX)teres[min(t+b,t)]=min(teres[min(t+b,t)],best+anscost[b]);
+            }
+            for(int j=0;j<=t;j++)anscost[j]=teres[j];
+            curspec++;
+        }
+        if(anscost[t]!=INT_MAX)ans=min(ans,fixedcost+anscost[t]);
+    }
+    if(ans==INT_MAX)printf("-1\n");
+    else printf("%d\n",ans);
     return 0;
 }
