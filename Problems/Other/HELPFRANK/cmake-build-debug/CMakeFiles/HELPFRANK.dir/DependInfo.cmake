3fbf68f3363499ae35216791723a9dac1347ce23
PeiAllen <32986987+PeiAllen@users.noreply.github.com>
2022-04-29 18:56:17 -0400

    puyshy

--- Problems/Other/HELPFRANK/cmake-build-debug/CMakeFiles/HELPFRANK.dir/DependInfo.cmake
+++ Problems/Other/HELPFRANK/cmake-build-debug/CMakeFiles/HELPFRANK.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/allen/OneDrive/Documents/GitHub/Code/Problems/Other/HELPFRANK/main.cpp" "C:/Users/allen/OneDrive/Documents/GitHub/Code/Problems/Other/HELPFRANK/cmake-build-debug/CMakeFiles/HELPFRANK.dir/main.cpp.obj"
+  "C:/Users/allen/Documents/Github/Code/Problems/Other/HELPFRANK/main.cpp" "C:/Users/allen/Documents/Github/Code/Problems/Other/HELPFRANK/cmake-build-debug/CMakeFiles/HELPFRANK.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- Problems/Other/HELPFRANK/main.cpp
+++ Problems/Other/HELPFRANK/main.cpp
@@ -1,40 +1,84 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define ll long long
-#define int ll
-#define ld long double
-#define pii pair<int, int>
-#define f first
-#define s second
-#define boost() cin.tie(0), cin.sync_with_stdio(0)
-const int MN=1001;
-ll ans[MN];
-int32_t main() {
-    boost();
-//    for(int mod=2;mod<=100;mod++){
-        int mod=251;
-        int am=0;
-        vector<int> bad;
-        int cnt=0;
-        int totient=0;
-        for(int i=1;i<mod;i++){
-            if(__gcd(mod,i)==1)totient++;
+
+int N;
+int K;
+int H [1000001][2];
+int L [1000001];
+bool centroid [1000001];
+int sts [1000001];
+long long ans = INT_MAX;
+unordered_map<int, int> previousPath;
+vector<pair<int, int>> currentPath;
+vector<pair<int, long long>> adj [1000001];
+
+int dfs(int loc, int parent) {
+    sts[loc] = 1;
+    for (auto x: adj[loc]) {
+        if (x.first != parent) {
+            sts[loc] += dfs(x.first, loc);
         }
-        for(int i=2;i<=2;i++){
-            set<int> residue;
-            for(int j=0;j<mod;j++){
-                ll v=j;
-                for(int k=1;k<i;k++){
-                    v=v*j%mod;
-                }
-                residue.insert(v);
-            }
-            if(residue.size()<=(mod+1)/2){
-                printf("%d\n",(residue.size()));
-                for(auto x:residue)printf("%d ",x);
+    }
+    return sts[loc];
+}
+
+int getCentroid(int cur, int parent, int treeSize) {
+    for (auto x: adj[cur]) {
+        if (x.first != parent && !centroid[x.first]) {
+            if (sts[x.first] > treeSize) {
+                return getCentroid(x.first, cur, treeSize);
             }
         }
-//        printf("\%d\n",cnt);
-//    }
-    return 0;
+    }
+    return cur;
+}
+
+void dfspath(int cur, int parent, int depth, long long length) {
+    currentPath.push_back({depth, length});
+    for (auto x: adj[cur]) {
+        if (x.first != parent && !centroid[x.first]) {
+            dfspath(x.first, cur, depth+1, length + x.second);
+        }
+    }
+}
+
+void decomposeTree(int cur) {
+    int cent = getCentroid(cur, -1, dfs(cur, -1)/2);
+    centroid[cent] = true;
+    previousPath[0] = 0;
+    for (auto x :adj[cent]) {
+        if (!centroid[x.first]) {
+            currentPath.clear();
+            dfspath(x.first, cent, 1, x.second);
+            for (auto path: currentPath) {
+                if (previousPath.count(K-path.second) > 0) ans = min(ans, (long long) path.first + previousPath[K-path.second]);
+            }
+            for (auto path: currentPath) {
+                if (previousPath.count(path.second) > 0)
+                    previousPath[path.second] = min(previousPath[path.second], path.first);
+                else
+                    previousPath[path.second] = path.first;
+            }
+        }
+    }
+    previousPath.clear();
+    for (auto x: adj[cent]) {
+        if (!centroid[x.first]) {
+            decomposeTree(x.first);
+        }
+    }
+}
+
+int best_path(int n, int k, int h [][2], int l []) {
+    for (int i = 0; i < n-1; i++) {
+        adj[h[i][0]].push_back({h[i][1], l[i]});
+        adj[h[i][1]].push_back({h[i][0], l[i]});
+    }
+    N = n;
+    K = k;
+    decomposeTree(1);
+
+    if (ans != INT_MAX) return ans;
+    return -1;
 }
